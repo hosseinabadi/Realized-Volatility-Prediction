3eaaa76a54d5c185dc02b15c04af8b7ebc4c186e
Amirmahdi <hosseinabadi26@gmail.com>
2024-06-03 10:41:18 +0200

    Models and Data Visualization updated

--- utils.py
+++ utils.py
@@ -2,68 +2,12 @@ from statsmodels.tsa.stattools import adfuller
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import numpy as np
-from scipy.stats import skew, kurtosis, norm, shapiro
-import seaborn as sns
-from scipy import stats
 
-
-def check_t_distribution(std_resid):
-    # Fit a t-distribution to the standardized residuals
-    t_params = stats.t.fit(std_resid)
-
-    # Perform Kolmogorov-Smirnov test
-    ks_stat, p_value = stats.kstest(std_resid, 't', t_params)
-
-    print(f"KS-statistic: {ks_stat}")
-    print(f"P-value: {p_value}")
-
-    # If the p-value is greater than 0.05, we cannot reject the null hypothesis that the data follows a t-distribution
-    if p_value > 0.05:
-        print("The standardized residuals could be from a t-distribution.")
-    else:
-        print("The standardized residuals are not from a t-distribution.")
-
-
-def check_normality(data):
-    skewness = skew(data)
-    kurt = kurtosis(data)
-
-    # 2. Visualize the distribution and plot the PDF of the normal distribution
-    plt.figure(figsize=(10, 6))
-
-    # Plot the data's PDF
-    sns.histplot(data, kde=True, color='blue', bins=30, stat='probability', label='Data PDF')
-
-    # Generate normal distribution data with the same mean and standard deviation
-    mu, sigma = data.mean(), data.std()
-    x = np.linspace(data.min(), data.max(), 1000)
-    pdf = norm.pdf(x, mu, sigma)  # Calculate the PDF of the normal distribution
-    # Normalize the PDF
-
-    # 3. Perform normality tests
-    shapiro_test = shapiro(data)
-    p_value = shapiro_test[1]
-
-    # Print results
-    print(f"Skewness: {skewness}", "Skewness of a normal distribution is 0.")
-    print(f"Kurtosis: {kurt}", "Kurtosis of a normal distribution is 3.")
-    print(f"Shapiro-Wilk Test p-value: {p_value}")
-
-    # Assess normality based on p-value
-    if p_value > 0.05:
-        print("The data could be assumed to be approximately normally distributed.")
-    else:
-        print("The data significantly deviates from a normal distribution.")
-
-    # Plot the normal distribution's PDF
-    plt.plot(x, pdf, 'r', label='Normal Distribution')
-
-    plt.title('Distribution of Data with Normal Distribution')
-    plt.xlabel('Values')
-    plt.ylabel('Probability Density')
-    plt.legend()
-    plt.show()
+'''
+Plot the ACF and PACF of a time series
+'''
 
 
 def plot_autocorrelations(columns, series_list, lags=30):
@@ -104,3 +48,69 @@ def transform_to_float(df):
 def Ljung_Box_Test(data, lags):
     result = sm.stats.acorr_ljungbox(data, lags=lags, return_df=True)
     return result
+
+
+# %%
+def calculate_aic_bic(y_true, y_pred, num_predictors, num_samples):
+    # Calculate RSS
+    rss = np.sum((y_true - y_pred) ** 2)
+
+    # Estimate the variance of the error
+    sigma_squared = rss / num_samples
+
+    # Calculate the likelihood
+    likelihood = np.prod(1 / np.sqrt(2 * np.pi * sigma_squared) * np.exp(-(y_true - y_pred) ** 2 / (2 * sigma_squared)))
+
+    # Calculate AIC
+    aic = 2 * num_predictors - 2 * np.log(likelihood)
+
+    # Calculate BIC
+    bic = num_samples * np.log(rss / num_samples) + num_predictors * np.log(num_samples)
+
+    return aic, bic
+
+
+# %%
+def calculate_cp(y_true, y_pred, num_predictors, num_samples):
+    # Calculate RSS
+    rss = np.sum((y_true - y_pred) ** 2)
+
+    # Estimate the variance of the error
+    sigma_squared = rss / (num_samples - num_predictors - 1)
+
+    # Calculate C_p
+    cp = (1 / num_samples) * (rss + 2 * num_predictors * sigma_squared)
+
+    return cp
+
+
+# %%
+def calculate_adjusted_r2(y_true, y_pred, num_predictors, num_samples):
+    # Calculate R^2
+    r2 = r2_score(y_true, y_pred)
+
+    # Calculate adjusted R^2
+    adjusted_r2 = 1 - (1 - r2) * ((num_samples - 1) / (num_samples - num_predictors - 1))
+
+    return adjusted_r2
+
+
+def mean_percentage_error(actual, predicted):
+    """
+    Calculate Mean Percentage Error (MPE).
+
+    Parameters:
+    actual (list): List of actual values.
+    predicted (list): List of predicted values.
+
+    Returns:
+    float: Mean Percentage Error (MPE).
+    """
+    # Calculate absolute percentage error for each pair of actual and predicted values
+    absolute_percentage_error = [abs((actual_val - pred_val) / actual_val) for actual_val, pred_val in
+                                 zip(actual, predicted)]
+
+    # Calculate mean of absolute percentage error
+    mpe = sum(absolute_percentage_error) / len(absolute_percentage_error)
+
+    return mpe
